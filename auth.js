0bdc9bf43434a8462495786b86a9ee543abf858d
kostya3802001 <join.the.game.notik.fols@gmail.com>
2026-01-19 12:37:06 +0200

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,27 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { 
+  getAuth, 
+  onAuthStateChanged, 
+  setPersistence, 
+  browserLocalPersistence 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgAl6GL0DQOhE-JpPAABzb4yvOctfpXXI",
+  authDomain: "d-print-studio-25787.firebaseapp.com",
+  projectId: "d-print-studio-25787",
+  storageBucket: "d-print-studio-25787.firebasestorage.app",
+  messagingSenderId: "671010955757",
+  appId: "1:671010955757:web:633317a7073d5d6f6c07e4"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+setPersistence(auth, browserLocalPersistence);
+
+export function requireAuth(callback){
+  onAuthStateChanged(auth, user => {
+    callback(user);
+  });
+}
